f6cea35b902e8b22077d14a198a617b4d4b4767f
dzuhri1234 <81511326+dzuhri1234@users.noreply.github.com>
2026-08-16 23:32:22 +0800

    Update modal.ts

--- src/views/modal.ts
+++ src/views/modal.ts
@@ -5,7 +5,7 @@ import { escapeHTML } from "../lib/utils";
 export function renderModal(title: string, bodyHtml: string): string {
   return `
     <div class="${modalOverlay}" data-action="closeModal" data-overlay="1">
-      <div class="${modalBox}" onclick="event.stopPropagation()">
+      <div class="${modalBox}" >
         <div class="${modalHeader}">
           <h3 class="${css({ fontWeight: "bold", color: "slate.800" })}">${escapeHTML(title)}</h3>
           <button data-action="closeModal" class="${css({ color: "slate.400", _hover: { color: "slate.700" }, fontSize: "xl", cursor: "pointer", lineHeight: "1" })}">&times;</button>
